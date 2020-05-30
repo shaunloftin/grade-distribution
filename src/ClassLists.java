323d17ba275f8ee99654a2a034d14074c4ab4241
landenfire <marvelmanthor@gmail.com>
2020-05-29 23:49:22 -0400

    Added comments because I forgot

--- src/ClassLists.java
+++ src/ClassLists.java
@@ -13,10 +13,19 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+/**
+ * Helper class that creates a Map of class names with their unabbreviated form,
+ * and an ArrayList of Map entries that store the School name in the Key and a
+ * list of departments in that school in the value.
+ * 
+ * @author Landen Master
+ * @author Shaun Loftin
+ *
+ */
 public class ClassLists {
 	
-	public static final String NAME_LIST = "data/Unabbreviated_Names.csv";
-	public static final String DEPARTMENT_LIST = "data/List_of_Departments.xlsx";
+	public static final String NAME_LIST = "data/Unabbreviated_Names.csv"; //CSV file of abbreviated names in the first column and their full names in the second column
+	public static final String DEPARTMENT_LIST = "data/List_of_Departments.xlsx"; //Excel file of school names in the first column and all departments in that school in subsequent columns 
 	
 	/**
 	 * Helper function that stores a map of all departments and their unabbreviated names.
@@ -40,6 +49,12 @@ public class ClassLists {
 	    return unabbreviatedClasses; //returning the full map
 	}
 	
+	/**
+	 * 
+	 * @param args
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
 	public static void main(String args[]) throws FileNotFoundException, IOException {
 		ArrayList<AbstractMap.SimpleEntry<String, ArrayList<String>>> departmentByCollege = departmentByCollege();
 		HashMap<String, String> unabbreviatedClasses = unabbreviatedClasses();
@@ -65,25 +80,33 @@ public class ClassLists {
 	public static final ArrayList<AbstractMap.SimpleEntry<String, ArrayList<String>>> departmentByCollege() throws FileNotFoundException, IOException {
 		XSSFWorkbook excelBook = new XSSFWorkbook(new FileInputStream(DEPARTMENT_LIST)); // Copying the excel workbook into a local variable
         XSSFSheet mainSheet = excelBook.getSheet("Sheet1"); // Copying the first excel sheet into a local variable
-        XSSFRow currentRow;
+        XSSFRow currentRow; //current row being read in of school and departments
         
 		ArrayList<AbstractMap.SimpleEntry<String, ArrayList<String>>> departmentByCollege = new ArrayList<AbstractMap.SimpleEntry<String, ArrayList<String>>>(); // ArrayList that stores each School and each of their departments
-		String currentCollege; //curent college being read in
+		String currentCollege; //current college being read in
 
-		System.out.println(mainSheet.getPhysicalNumberOfRows());
-		
+		//going through each row and getting all departments
 		for (int i = 0; i < mainSheet.getPhysicalNumberOfRows(); i++) {
-			currentRow = mainSheet.getRow(i);
+			
+			currentRow = mainSheet.getRow(i); //getting current row to read in
 			ArrayList<String> currentSchoolDepartments = new ArrayList<String>(); //all departments of current college being read in
-			currentCollege = currentRow.getCell(0).toString();
+			currentCollege = currentRow.getCell(0).toString(); //Official college name being read in
+			
+			//reading in each abbreviated department name until there are no more departments in the row
 			for (int k = 1; k < currentRow.getLastCellNum(); k++) {
 				currentSchoolDepartments.add(currentRow.getCell(k).toString());
 			}
+			
+			//creating a Map entry to store the college and its departments
 			AbstractMap.SimpleEntry<String, ArrayList<String>> currentSchoolAndDepartments = new AbstractMap.SimpleEntry<String, ArrayList<String>>(currentCollege, currentSchoolDepartments);
+			
+			//adding current school with its department to the final list
 			departmentByCollege.add(currentSchoolAndDepartments);
 		}
-		excelBook.close();
-		return departmentByCollege;
+		
+		excelBook.close(); //closing the instream of the excel book
+		
+		return departmentByCollege; //returning an ArrayList of Map entrys of School names and all abbreviated department names
 	}
 
 	
