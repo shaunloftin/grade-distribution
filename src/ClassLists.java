c77226b8d5ee0d2ad51fa0474917ebe2783991c1
landenfire <marvelmanthor@gmail.com>
2020-05-29 23:40:07 -0400

    fixed and finished departmentByCollege, main method of ClassLists now displays each college and all departments

--- src/ClassLists.java
+++ src/ClassLists.java
@@ -41,32 +41,19 @@ public class ClassLists {
 	}
 	
 	public static void main(String args[]) throws FileNotFoundException, IOException {
-		Scanner keyboard = new Scanner(System.in);
-		System.out.print("Type 1 to test unabbreviatedClasses, 2 to test deptByCollege: ");
-		String input = keyboard.nextLine();
-		
-		while (!input.equals("1") && !input.equals("2")) {
-			System.err.println("ERROR: Expected 1 or 2.");
-			System.out.print("Type 1 to test unabbreviatedClasses, 2 to test deptByCollege: ");
-			input = keyboard.nextLine();
-		}
-		
-		if (input.equals("1") ) {
-			HashMap<String, String> data = unabbreviatedClasses();
-			for (Map.Entry<String, String> entry : data.entrySet()) {
-				System.out.println(entry.getKey() + "\t" + entry.getValue());
+		ArrayList<AbstractMap.SimpleEntry<String, ArrayList<String>>> departmentByCollege = departmentByCollege();
+		HashMap<String, String> unabbreviatedClasses = unabbreviatedClasses();
+		String currentDepartment;
+		for (int i = 0 ; i < departmentByCollege.size(); i++) {
+			AbstractMap.SimpleEntry<String, ArrayList<String>> currentSchool = departmentByCollege.get(i);
+			System.out.println(currentSchool.getKey() + ": ");
+			ArrayList<String> departments = currentSchool.getValue();
+			for (int k = 0; k < departments.size(); k++) {
+				currentDepartment = unabbreviatedClasses.get(departments.get(k));
+				System.out.print(currentDepartment + ", ");
 			}
-		} else {
-			ArrayList<Map.Entry<String, ArrayList<String>>> data = departmentByCollege();
-			for (Map.Entry<String, ArrayList<String>> college : data) {
-				System.out.print(college.getKey() + " - ");
-				for (String department : college.getValue()) {
-					System.out.print(department + " ");
-				}
-			}
+			System.out.println("\n");
 		}
-		
-		keyboard.close();
 	}
 	
 	
@@ -75,39 +62,26 @@ public class ClassLists {
 	 * @return
 	 * 			Map with keys of abbreviated classes and values of what college they fall under.
 	 */
-	public static final ArrayList<Map.Entry<String, ArrayList<String>>> departmentByCollege() throws FileNotFoundException, IOException {
+	public static final ArrayList<AbstractMap.SimpleEntry<String, ArrayList<String>>> departmentByCollege() throws FileNotFoundException, IOException {
 		XSSFWorkbook excelBook = new XSSFWorkbook(new FileInputStream(DEPARTMENT_LIST)); // Copying the excel workbook into a local variable
         XSSFSheet mainSheet = excelBook.getSheet("Sheet1"); // Copying the first excel sheet into a local variable
         XSSFRow currentRow;
         
-		ArrayList<Map.Entry<String, ArrayList<String>>> departmentByCollege = new ArrayList<Map.Entry<String, ArrayList<String>>>(); // ArrayList that stores each School and each of their departments
-		// TODO - read in excel file
-		String currentSchool; // current school being read
-		ArrayList<String> currentSchoolDepartments = new ArrayList<String>(); // all departments in current school
+		ArrayList<AbstractMap.SimpleEntry<String, ArrayList<String>>> departmentByCollege = new ArrayList<AbstractMap.SimpleEntry<String, ArrayList<String>>>(); // ArrayList that stores each School and each of their departments
+		String currentCollege; //curent college being read in
+
+		System.out.println(mainSheet.getPhysicalNumberOfRows());
 		
-		// TODO - fix bug when reading row with dept of military sci
-		/*
-		 * for (int i = 0; i < excel rows; i++) {
-		 * 		currentSchoolDepartments.clear();
-		 * 		currentSchool = first cell of current row;
-		 * 		while (!next cell is empty) {
-		 * 			currentSchoolDepartments.put(current cell);
-		 * 		}
-		 * }
-		 */
-		
-		for (int i = 0; i < mainSheet.getLastRowNum(); i++) {
-			currentSchoolDepartments.clear();
+		for (int i = 0; i < mainSheet.getPhysicalNumberOfRows(); i++) {
 			currentRow = mainSheet.getRow(i);
-			currentSchool = currentRow.getCell(0).toString();
-			
-			for (int k = 1; k < (int)(currentRow.getLastCellNum())-1; k++) {	
-				System.out.println(currentSchool + ", " + currentRow.getCell(k).toString() + " last cell: " + ((int)(currentRow.getLastCellNum())-1));
+			ArrayList<String> currentSchoolDepartments = new ArrayList<String>(); //all departments of current college being read in
+			currentCollege = currentRow.getCell(0).toString();
+			for (int k = 1; k < currentRow.getLastCellNum(); k++) {
 				currentSchoolDepartments.add(currentRow.getCell(k).toString());
 			}
-			departmentByCollege.add(new AbstractMap.SimpleEntry<String, ArrayList<String>>(currentSchool, currentSchoolDepartments));
+			AbstractMap.SimpleEntry<String, ArrayList<String>> currentSchoolAndDepartments = new AbstractMap.SimpleEntry<String, ArrayList<String>>(currentCollege, currentSchoolDepartments);
+			departmentByCollege.add(currentSchoolAndDepartments);
 		}
-		
 		excelBook.close();
 		return departmentByCollege;
 	}
